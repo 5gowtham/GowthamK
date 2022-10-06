93625b26b1c3fae4d0059fd47c7cd7dd35a5d459
Gowtham <gowtham.k@survey2connect.com>
2022-10-06 18:52:40 +0530

    file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import Todo from './components/Todo';
 import './App.css';
+
 
 function App() {
   return (
-    <div>Hello world</div>
+    <div>
+      <h1>My Todos</h1>
+      <Todo text='React'/>
+      <Todo text='Node'/>
+      <Todo text='Angular'/>
+   
+      </div>
   );
 }
 
